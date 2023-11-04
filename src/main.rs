8ba978656bbcb23cad4803a67bacd87f7e779b1d
luansapelli <luansapelli@gmail.com>
2023-11-03 22:36:28 -0300

    add player attack and collision with asteroids

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use bevy::prelude::*;
-use bevy::window::{PresentMode, PrimaryWindow};
 use bevy::audio::{PlaybackMode, Volume};
-use bevy::time::TimerMode::{Once};
+use bevy::prelude::*;
+use bevy::time::TimerMode::Once;
+use bevy::window::{PresentMode, PrimaryWindow};
 
 fn main() {
     App::new()
@@ -18,6 +18,7 @@ fn main() {
             EnvironmentPlugin,
             PlayerPlugin,
             AsteroidPlugin,
+            AttackPlugin,
             CollisionPlugin,
         ))
         .run();
@@ -25,6 +26,7 @@ fn main() {
 
 #[derive(Component)]
 struct Environment;
+
 struct EnvironmentPlugin;
 
 impl Plugin for EnvironmentPlugin {
@@ -51,7 +53,7 @@ fn spawn_environment(
             ..default()
         },
         Environment,
-        ));
+    ));
 
     commands.spawn(AudioBundle {
         source: asset_server.load("audio/background.ogg"),
@@ -62,20 +64,30 @@ fn spawn_environment(
         },
         ..default()
     });
-
 }
+
 const PLAYER_SPEED: f32 = 600.0;
 const PLAYER_SIZE: f32 = 64.0;
 
 #[derive(Component)]
 struct Player;
+
 struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (spawn_player, spawn_camera))
+        app.add_systems(Startup, (spawn_camera, spawn_player))
             .add_systems(Update, (player_movement, confine_player_movement));
     }
+}
+
+fn spawn_camera(window_query: Query<&Window, With<PrimaryWindow>>, mut commands: Commands) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
 
 fn spawn_player(
@@ -97,15 +109,6 @@ fn spawn_player(
         },
         Player,
     ));
-}
-
-fn spawn_camera(window_query: Query<&Window, With<PrimaryWindow>>, mut commands: Commands) {
-    let window = window_query.get_single().unwrap();
-
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-        ..default()
-    });
 }
 
 fn player_movement(
@@ -140,6 +143,7 @@ fn player_movement(
     }
 }
 
+
 fn confine_player_movement(
     mut player_query: Query<&mut Transform, With<Player>>,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -171,12 +175,74 @@ fn confine_player_movement(
     }
 }
 
+const PLAYER_ATTACK_SPEED: f32 = 800.0;
+
+#[derive(Component)]
+struct Attack;
+
+struct AttackPlugin;
+
+impl Plugin for AttackPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, (player_attack, player_attack_movement));
+    }
+}
+
+fn player_attack(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&Transform, With<Player>>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+) {
+    if let Ok(player_transform) = player_query.get_single_mut() {
+        if keyboard_input.just_pressed(KeyCode::Space) {
+            commands.spawn(AudioBundle {
+                source: asset_server.load("audio/laser_shot.ogg"),
+                settings: PlaybackSettings {
+                    volume: Volume::new_absolute(0.3),
+                    ..default()
+                },
+                ..default()
+            });
+
+            commands.spawn((
+                SpriteBundle {
+                    transform: Transform::from_xyz(
+                        player_transform.translation.x,
+                        player_transform.translation.y,
+                        0.0,
+                    ),
+                    texture: asset_server.load("sprites/laser_shot.png"),
+                    ..default()
+                },
+                Attack,
+            ));
+        }
+    }
+}
+
+fn player_attack_movement(
+    mut attack_query: Query<&mut Transform, With<Attack>>,
+    time: Res<Time>,
+) {
+    for mut transform in attack_query.iter_mut() {
+        let mut direction = Vec3::ZERO;
+
+        direction.y += 1.0;
+
+        transform.translation += direction * PLAYER_ATTACK_SPEED * time.delta_seconds();
+    }
+}
 
 const ASTEROID_SPEED: f32 = 200.0;
-const SPAWN_INTERVAL:  f32 = 0.5;
+const ASTEROID_SIZE: f32 = 64.0;
+const SPAWN_INTERVAL: f32 = 0.5;
+
 #[derive(Component)]
 struct Asteroid;
+
 struct AsteroidPlugin;
+
 #[derive(Resource)]
 struct AsteroidSpawnState {
     timer: Timer,
@@ -192,8 +258,7 @@ impl Default for AsteroidSpawnState {
 
 impl Plugin for AsteroidPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, asteroid_movement)
-            .add_systems(Update, spawn_asteroid)
+        app.add_systems(Update, (asteroid_movement, spawn_asteroid))
             .insert_resource(AsteroidSpawnState::default());
     }
 }
@@ -225,7 +290,10 @@ fn spawn_asteroid(
     }
 }
 
-fn asteroid_movement(mut asteroid_query: Query<&mut Transform, With<Asteroid>>, time: Res<Time>) {
+fn asteroid_movement(
+    mut asteroid_query: Query<&mut Transform, With<Asteroid>>,
+    time: Res<Time>
+) {
     for mut transform in asteroid_query.iter_mut() {
         let mut direction = Vec3::ZERO;
 
@@ -237,11 +305,12 @@ fn asteroid_movement(mut asteroid_query: Query<&mut Transform, With<Asteroid>>, 
 
 #[derive(Component)]
 struct Collision;
+
 struct CollisionPlugin;
 
 impl Plugin for CollisionPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, asteroid_hits_player);
+        app.add_systems(Update, (asteroid_hits_player, attack_hits_asteroid));
     }
 }
 
@@ -267,6 +336,34 @@ fn asteroid_hits_player(
                     ..default()
                 });
                 commands.entity(player_entity).despawn();
+            }
+        }
+    }
+}
+
+fn attack_hits_asteroid(
+    mut asteroid_query: Query<(Entity, &Transform), With<Asteroid>>,
+    mut attack_query: Query<(Entity, &Transform), With<Attack>>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+) {
+    for (asteroid_entity, asteroid_transform) in asteroid_query.iter_mut() {
+        for (attack_entity, attack_transform) in attack_query.iter_mut() {
+            let distance = attack_transform
+                .translation
+                .distance(asteroid_transform.translation);
+
+            if distance < ASTEROID_SIZE - 20.0 {
+                commands.spawn(AudioBundle {
+                    source: asset_server.load("audio/explosion.ogg"),
+                    settings: PlaybackSettings {
+                        volume: Volume::new_absolute(0.3),
+                        ..default()
+                    },
+                    ..default()
+                });
+                commands.entity(asteroid_entity).despawn();
+                commands.entity(attack_entity).despawn();
             }
         }
     }
